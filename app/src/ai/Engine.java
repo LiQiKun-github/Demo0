1e0a8ff0dcbb678854472826290eb9f7a4d2b8fb
webturing <zj@webturing.com>
2020-06-22 16:16:26 +0800

    24点游戏求解引擎设计完毕，算法还未实现

---
+++ app/src/ai/Engine.java
@@ -0,0 +1,13 @@
+package ai;
+
+public class Engine {
+    public static void main(String[] args) {
+        System.out.println(solve24(1,2,3,4));
+    }
+
+    public static String solve24(int a, int b, int c, int d) {
+
+        //TODO;
+        return "1*2*3*4=24";
+    }
+}
