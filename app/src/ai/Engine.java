892736aba48078cf4e20feb394581933dcc9a726
webturing <zj@webturing.com>
2020-06-29 15:38:19 +0800

    设计了随机求解器

--- app/src/ai/Engine.java
+++ app/src/ai/Engine.java
@@ -1,16 +1,55 @@
 package ai;
 
-public class Engine {
-    public static void main(String[] args) {
-        System.out.println(solve24(1,2,3,4));
-        int [] a={1,5,5,5};
-        String[] op={"*","-","/"};
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
 
+public class Engine {
+    private static final int MAX_RAND_SEARCH_COUNT = 5040;
+    final static String[] OPS = "+ - * /".split(" ");
+
+    public static void main(String[] args) {
+        System.out.println(solve24(1, 2, 3, 4));
+        System.out.println(randSearcher(new int[]{1, 5, 5, 5}));
+        System.out.println(randSearcher(new int[]{1, 2, 3, 4}));
+        System.out.println(randSearcher(new int[]{1, 5, 7, 1}));
+        System.out.println(randSearcher(new int[]{1, 1, 1, 1}));
+    }
+
+    private static String randSearcher(int[] a) {
+        for (int j = 0; j < 4 * 4 * 4; j++) {
+            String[] ops = new String[3];
+            int x = j / 16;
+            int y = j % 16 / 4;
+            int z = j % 4;
+            ops[0] = OPS[x];
+            ops[1] = OPS[y];
+            ops[2] = OPS[z];
+
+            List<String> exp = new Vector<>();
+            for (Integer i : a) {
+                exp.add(Integer.toString(i));
+            }
+            for (String op : ops) {
+                exp.add(op);
+
+            }
+
+            int tot = 0;
+            while (++tot < MAX_RAND_SEARCH_COUNT) {
+                Collections.shuffle(exp);
+                double result = RPolandExpression.eval(exp);
+                if (result == 24) {
+                    return (exp).toString();
+                }
+            }
+        }
+        return "No solution";
     }
 
     public static String solve24(int a, int b, int c, int d) {
 
-        //TODO;
-        return "1*2*3*4=24";
+
+        return randSearcher(new int[]{a,b,c,d});
     }
 }
